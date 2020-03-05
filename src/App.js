f67aba17ba230bfb06853042b0b27464af5f06b9
Tina <tina.jston@gmail.com>
2020-03-05 14:48:47 +0000

    changes to react text

--- src/App.js
+++ src/App.js
@@ -8,7 +8,7 @@ function App() {
       <header className='App-header'>
         <img src={logo} className='App-logo' alt='logo' />
         <p>
-          <h1>Changes on feature branch</h1>
+          <h1>Changes on feature branch again</h1>
         </p>
         <a
           className='App-link'
